b8228584b99c3212b1f4d959f2cc2a6dc9c43774
K. Shankari <shankari@eecs.berkeley.edu>
2016-02-22 00:57:45 -0800

    Move the call to the server into a separate thread
    So that it does not block the webcore thread

--- src/android/CommunicationHelperPlugin.java
+++ src/android/CommunicationHelperPlugin.java
@@ -11,18 +11,26 @@ import edu.berkeley.eecs.emission.cordova.connectionsettings.ConnectionSettings;
 
 public class CommunicationHelperPlugin extends CordovaPlugin {
     @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
         if (action.equals("pushGetJSON")) {
             try {
-                Context ctxt = cordova.getActivity();
+                final Context ctxt = cordova.getActivity();
                 String relativeURL = data.getString(0);
-                JSONObject filledMessage = data.getJSONObject(1);
+                final JSONObject filledMessage = data.getJSONObject(1);
 
                 String commuteTrackerHost = ConnectionSettings.getConnectURL(ctxt);
-                String fullURL = commuteTrackerHost + relativeURL;
+                final String fullURL = commuteTrackerHost + relativeURL;
 
+                cordova.getThreadPool().execute(new Runnable() {
+                    public void run() {
+                        try {
                 String resultString = CommunicationHelper.pushGetJSON(ctxt, fullURL, filledMessage);
                 callbackContext.success(new JSONObject(resultString));
+            } catch (Exception e) {
+                callbackContext.error("While pushing/getting from server "+e.getMessage());
+            }
+                    }
+                });
             } catch (Exception e) {
                 callbackContext.error("While pushing/getting from server "+e.getMessage());
             }
